03780c3725bcbee053da615da96811939b20fbf9
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2020-12-16 13:16:25 -0700

    done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,7 @@
+require 'pry'
 Dir[File.join(File.dirname(__FILE__), "../lib", "*.rb")].each {|f| require f}
+
+#lior = Owner.new("lior")
+#arielle = Owner.new("Arielle")
+#milky= Cat.new("milky", lior)
+#kitty=Cat.new("Kitty", arielle)
